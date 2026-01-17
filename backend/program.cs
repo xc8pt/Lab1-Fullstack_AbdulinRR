163b6c8299a8aa428375ed969d101192102af13b
xc8pt <brolike636@gmail.com>
2026-01-17 17:57:34 +0300

    Input name

--- backend/program.cs
+++ backend/program.cs
@@ -1,7 +1,3 @@
-﻿using System.Data;
-
-string priv = "Приветики писталетики";
-string name = "Абдулин Ринат Рушанович";
-DateTime g = DateTime.Now;
-Console.WriteLine($"{priv}\n{name}\n{g}");
+﻿Console.Write("Напишите свой ФИО: ");
+string? name = Console.ReadLine();
 
